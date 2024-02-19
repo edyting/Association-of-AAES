b50e1986c8e71f726959f0fc85bafa01ae068f1c
manuel <dankyiemmanuel0@gmail.com>
2024-02-19 14:51:04 +0000

    working on contact page

--- client/src/pages/contact/Second.jsx
+++ client/src/pages/contact/Second.jsx
@@ -2,40 +2,40 @@ import React from 'react'
 
 function Second() {
   return (
-    <div className='bg-blue-200'>
+    <div className=' flex '>
      {/* leftside */}
 
-      <div className=" ">
+      <div className="bg-muted w-[25rem] p-4 rounded-lg m-2">
         {/* contact us */}
       <div className="">
-        <h1>Contact Us </h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quae dolorum quos iure iusto dolore?
+        <h1 className='font-bold text-lg'>Contact Us </h1>
+        <p className='text-grey-300 text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati quae dolorum quos iure iusto dolore?
         </p>
       </div>
       {/* info */}
       <div className="">
         {/* address */}
         <div className="">
-            <h1 className='font-bold'>Address:</h1>
-            <p>Knust campus</p>
+            <h1 className='font-bold text-lg'>Address:</h1>
+            <p  className='text-grey-300 text-sm'>Knust campus</p>
         </div>
         {/* phone */}
         <div className="">
-            <h1 className='font-bold'>Phone:</h1>
-            <p>+233 4040440400</p>
+            <h1 className='font-bold text-lg'>Phone:</h1>
+            <p  className='text-grey-300 text-sm'>+233 4040440400</p>
         </div>
         {/* working ours */}
         <div className="">
-            <h1 className='font-bold'>We are open on:</h1>
+            <h1 className='font-bold text-lg'>We are open on:</h1>
             <p>Monday-Friday</p>
-            <p>8:00am-7:00pm</p>
+            <p className='text-grey-300 text-sm'>8:00am-7:00pm</p>
             <p>Saturday:</p>
-            <p>10:30am-5:30pm</p>
+            <p className='text-grey-300 text-sm'>10:30am-5:30pm</p>
         </div>
         {/* email */}
         <div className="">
-            <h1 className='font-bold'>E-mail:</h1>
-            <a href='#'>logo@gmail.com</a>
+            <h1 className='font-bold text-lg'>E-mail:</h1>
+            <a href='#'  className='text-grey-300 text-sm'>logo@gmail.com</a>
         </div>
       </div>
       </div>
